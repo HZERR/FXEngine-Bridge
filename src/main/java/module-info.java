6966e18da4a207fa138da2340bed971806c035f5
HZERR <vadimvyazloy@yandex.ru>
2024-04-22 01:54:40 +0300

    Add module file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module FXEngine.Bridge {
+    requires javafx.graphics;
+    requires spring.core;
+    requires CommonInstruments.JDK17;
+    requires spring.beans;
+    requires spring.context;
+    requires ch.qos.logback.classic;
+    requires org.slf4j;
+    requires ch.qos.logback.core;
+    requires org.jetbrains.annotations;
+    requires typesafe.config;
+    requires javafx.fxml;
+    requires javafx.controls;
+}
