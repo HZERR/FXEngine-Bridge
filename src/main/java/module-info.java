47736ab203bafa9e5fc93f61d9c3888505b72db1
HZERR <vadimvyazloy@yandex.ru>
2024-04-23 02:00:59 +0300

    Add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module FXEngine.Bridge {
+    requires spring.core;
+    requires CommonInstruments.JDK17;
+    requires spring.beans;
+    requires spring.context;
+    requires ch.qos.logback.classic;
+    requires org.slf4j;
+    requires ch.qos.logback.core;
+    requires org.jetbrains.annotations;
+    requires typesafe.config;
+}
