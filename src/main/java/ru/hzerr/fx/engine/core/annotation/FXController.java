c26d6cbe0df6031d76706abdee85f2cf35a146b2
HZERR <vadimvyazloy@yandex.ru>
2024-04-24 03:44:35 +0300

    Fix annotation

--- src/main/java/ru/hzerr/fx/engine/core/annotation/FXController.java
+++ src/main/java/ru/hzerr/fx/engine/core/annotation/FXController.java
@@ -6,7 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @RegisteredPrototype
-@Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 public @interface FXController {
 }
