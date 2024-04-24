055b3b2c0a5b80a53cae1942b2efd5791e25fd57
HZERR <vadimvyazloy@yandex.ru>
2024-04-24 04:19:42 +0300

    Fix annotation

--- src/main/java/ru/hzerr/fx/engine/core/annotation/as/EngineLogProvider.java
+++ src/main/java/ru/hzerr/fx/engine/core/annotation/as/EngineLogProvider.java
@@ -6,6 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @IncludeAs("engineLogProvider")
+@Retention(RetentionPolicy.RUNTIME)
 public @interface EngineLogProvider {
     /**
      * Declares whether the annotated dependency is required.
